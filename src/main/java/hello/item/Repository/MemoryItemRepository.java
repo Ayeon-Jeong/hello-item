b264e18a153ca03ec8898fb9e460e92ddd5e8d50
Ayeon-Jeong <jdh961224@gmail.com>
2021-12-22 21:48:31 +0900

    20211222

--- src/main/java/hello/item/Repository/MemoryItemRepository.java
+++ src/main/java/hello/item/Repository/MemoryItemRepository.java
@@ -2,10 +2,17 @@ package hello.item.Repository;
 
 import hello.item.Domain.Item;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class MemoryItemRepository implements ItemRepository{
+
+
+    // 어딘가 저장할 공간이 필요 -> Map 사용
+    private static Map<Long, Item> store = new HashMap<>();
+    private static long sequence = 0L;
 
     @Override
     public List<Item> findAll() {
@@ -18,17 +25,21 @@ public class MemoryItemRepository implements ItemRepository{
     }
 
     @Override
+    // 아이템 등록
     public Item save(Item item) {
-        return null;
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
     }
 
     @Override
     public Optional<Item> findById(Long id) {
-        return Optional.empty();
+        return Optional.ofNullable(store.get(id));
     }
 
     @Override
     public Optional<Item> findByName(String name) {
-        return Optional.empty();
+        return store.values().stream().filter(
+                member -> member.getName().equals(name)).findAny();
     }
 }
